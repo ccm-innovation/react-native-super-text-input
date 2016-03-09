932044e1185f98aba1a4352f2925807bdc535d43
Brad Bumbalough <bbumbalough@tccm.org>
2016-03-08 22:41:29 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+let React = require('react-native')
+
+let {
+  Component,
+  TextInput,
+  PropTypes
+} = React
+
+class SuperTextInput extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      timeout: null
+    }
+  }
+
+  render() {
+    let {onPauseText, pauseDelay, onChangeText} = this.props
+    let {timeout} = this.state
+    return (
+      <TextInput
+         {...this.props}
+         onChangeText={(text) => {
+           if (onPauseText) {
+             if (timeout) window.clearTimeout(timeout)
+             timeout = window.setTimeout(() => onPauseText(), pauseDelay)
+             this.setState({timeout})
+           }
+           if (onChangeText) onChangeText(text)
+         }}
+       />
+    )
+  }
+
+}
+
+SuperTextInput.propTypes = {
+  onPauseText: PropTypes.func,
+  pauseDelay: PropTypes.number
+}
+
+SuperTextInput.defaultProps = {
+  pauseDelay: 500
+}
+
+module.exports = SuperTextInput
---
+++ readme.md
@@ -0,0 +1,15 @@
+# React Native Super TextInput
+
+> Super charged TextInput for React Native
+
+### Props
+All TextInput props
+
+### Super Props
+
+**onPauseText** function
+Calls the provided function `pauseDelay` milliseconds after the last `onChangeText`.
+
+**pauseDelay** number
+*Default: 500*
+The number of milliseconds to wait after the last text change before firing `onPauseText`.
